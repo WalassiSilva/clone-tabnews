7f3a46034d9b61ae3878903f4e954d9dee5605db
Walassi Silva <91761745+WalassiSilva@users.noreply.github.com>
2024-09-20 23:31:31 +0000

    fix: fix home page text

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <>
             <h1>Olá Pessoa!, Tudo bem com você?</h1>
+            <p>Trás carne pra fazer gostrognokof</p>
         </>
     )
 }
