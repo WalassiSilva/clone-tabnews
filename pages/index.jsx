c6e4f5e5eb3aa2777112f735df11d7cb0f0fe210
Walassi Silva <91761745+WalassiSilva@users.noreply.github.com>
2024-09-23 09:42:53 +0000

    fix: add default message

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,20 @@
 "use client"
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Home() {
+    const [value, setValue] = useState("");
     const res = ["",
         "Pena de urubu, pena de galinha... Se vocês está com dor no cu... Dá uma risadinha.",
         "Trás carne pra fazer gostrognokof",
         "yabirifó (e pau). yabirifó (e rola)",
     ]
-    const [value, setValue] = useState();
     return (
         <>
             <h1>Escolha entre "1", "2" e "3"</h1>
-            <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
-            <p>{res[value]}</p>
+            <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+            {/* <p>{value !== "1" || value !== "2" || value !== "3"  ? "Olá! Estou aqui pra estudar no curso.dev" : res[value]}</p> */}
+            <p>{!value || +value > res.length-1 && "Olá! Estou aqui pra estudar no curso.dev"}</p>
+            <p>{value && res[value]}</p>
         </>
     )
 }
