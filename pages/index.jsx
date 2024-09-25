2573b4fee322ca30915e49ecb7299e2a61539f01
Walassi Silva <91761745+WalassiSilva@users.noreply.github.com>
2024-09-25 03:00:38 +0000

    chore: add `lint:fix` and `lint:write` scripts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,34 @@
-"use client"
+"use client";
 import { useState } from "react";
 
 export default function Home() {
-    const [value, setValue] = useState("");
-    const res = ["",
-        "Pena de urubu, pena de galinha... Se vocês está com dor no cu... Dá uma risadinha.",
-        "Trás carne pra fazer gostrognokof",
-        "yabirifó (e pau). yabirifó (e rola)",
-    ]
-    return (
-        <>
-            <h1>Escolha entre "1", "2" e "3"</h1>
-            <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-            {/* <p>{value !== "1" || value !== "2" || value !== "3"  ? "Olá! Estou aqui pra estudar no curso.dev" : res[value]}</p> */}
-            <p>{!value || +value > res.length-1 && "Olá! Estou aqui pra estudar no curso.dev"}</p>
-            <p>{value && res[value]}</p>
-        </>
-    )
+  const [value, setValue] = useState("");
+  const res = [
+    "",
+    "Pena de urubu, pena de galinha... Se vocês está com dor no cu... Dá uma risadinha.",
+    "Trás carne pra fazer gostrognokof",
+    "yabirifó (e pau). yabirifó (e rola)",
+  ];
+
+  function test() {
+    
+  }
+
+  return (
+    <>
+      <h1>Escolha entre "1", "2" e "3"</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {/* <p>{value !== "1" || value !== "2" || value !== "3"  ? "Olá! Estou aqui pra estudar no curso.dev" : res[value]}</p> */}
+      <p>
+        {!value ||
+          (+value > res.length - 1 &&
+            "Olá! Estou aqui pra estudar no curso.dev")}
+      </p>
+      <p>{value && res[value]}</p>
+    </>
+  );
 }
